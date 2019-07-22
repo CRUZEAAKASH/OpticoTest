39d675db669f2f66f0ac3783f9f300a734b673ef
aakash <aakash.shinghal@gmail.com>
2019-07-23 00:46:26 +0530

    test Data fIle added and @DataProvider anootation method is in progress

--- OpticoTest/src/main/java/com/optico/qa/util/TestUtil.java
+++ OpticoTest/src/main/java/com/optico/qa/util/TestUtil.java
@@ -1,5 +1,13 @@
 package com.optico.qa.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.optioc.qa.base.TestBase;
@@ -10,4 +18,29 @@ public class TestUtil extends TestBase {
 	public static long IMPLICIT_WAIT = 10;
 
 	public static WebDriverWait wait = new WebDriverWait(driver, 20);
+	public static String Test_Data_Sheet_Path = currentURL
+			+ "src\\main\\java\\com\\optico\\qa\\testdata\\TestData.xlsx";
+	public static Workbook book;
+	public static Sheet sheetName;
+	
+	public static Object[][] getTestData() {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(Test_Data_Sheet_Path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (EncryptedDocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheet)
+		
+	}
 }
